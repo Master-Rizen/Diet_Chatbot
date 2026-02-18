671ecd1ae0a6427edd7e5bed7e7e29bd254c6c41
Rizen <149863200+Master-Rizen@users.noreply.github.com>
2026-02-18 21:30:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import datetime
 import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 from pymongo import MongoClient
-from datetime import datetime,UTC
+from datetime import datetime
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
@@ -32,45 +33,44 @@ app.add_middleware(
 
 prompt = ChatPromptTemplate.from_messages(
     [
-        ("system","You are a diet specialist,give me the output accordingly"),
-
+        ("system", "you are a diet specialist, give me the output accordingly"),
+        ("placeholder", "{history}"),
         ("user", "{question}")
     ]
 )
 
-llm = ChatGroq(api_key = groq_api_key,model = "openai/gpt-oss-20b")
+llm = ChatGroq(api_key = groq_api_key, model="openai/gpt-oss-20b")
 chain = prompt | llm
 
-user_id = "user123"
-
 def get_history(user_id):
-    chats = collection.find({"user.id": user_id}).sort("timestamp",1)
+    chats = collection.find({"user_id": user_id}).sort("timestamp", 1)
     history = []
 
     for chat in chats:
-        history.append({chat["role"],chat["message"]})
+        history.append((chat["role"], chat["message"]))
     return history
 
-@app.get("/")
+@app.get("/") 
 def home():
-    return {"message" : "Welcome to the Diet Specialist ChatBot API"}
+    return {"message": "Welcome to the Diet Specialist Chatbot API!"}
 
 @app.post("/chat")
-def chat(request : ChatRequest):
-    history = get_history(user_id)
-    response = chain.invoke({"history": history,"question": request.question})
+def chat(request: ChatRequest):
+    history = get_history(request.user_id)
+    response = chain.invoke({"history": history, "question": request.question})
+
     collection.insert_one({
-    "user_id": request.user_id,
-    "role" : "user",
-    "message" : request.question,
-    "timestamp" : datetime.now(UTC)
+        "user_id": request.user_id,
+        "role": "user",
+        "message": request.question,
+        "timestamp": datetime.utcnow()
     })
 
     collection.insert_one({
         "user_id": request.user_id,
-        "role" : "assistant",
-        "message" : response.content,
-        "timestamp" : datetime.now(UTC)
+        "role": "assistant",
+        "message": response.content,
+        "timestamp": datetime.utcnow()
     })
 
-    return{"response" : response.content}
+    return {"response" : response.content}
